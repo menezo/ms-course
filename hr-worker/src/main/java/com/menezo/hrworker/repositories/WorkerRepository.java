463abb97efedf58d861bef70f7aebe8ec6ba35d3
Menezo <joaovitormenezesm12@gmail.com>
2023-05-02 17:10:17 -0300

    hr-worker implementation

---
+++ hr-worker/src/main/java/com/menezo/hrworker/repositories/WorkerRepository.java
@@ -0,0 +1,9 @@
+package com.menezo.hrworker.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.menezo.hrworker.entities.Worker;
+
+public interface WorkerRepository extends JpaRepository<Worker, Long>{
+
+}
